bc69c6311515c3a71633a836bf7e664c1cb031a1
Prakshal Shah <prakshalshah78@gmail.com>
2022-07-31 13:57:42 +0530

    31_07- Doubt Session

--- Code/ArmstrongNo.js
+++ Code/ArmstrongNo.js
@@ -2,20 +2,35 @@
 // find no of digits => 3
 // fetch every ind digits and multiply it with no of digits times => 1*1*1 + 5*5*5 + 3*3*3
 
+function checkArmstrong(number){ //153
+  var numOriginal = number; // 153
+  var digits = 0;
+  while(number > 0){ // 153>0 , 15>0, 1>0
+    digits = digits +1; //1 //2 //3
+    number = parseInt(number/10) //15 //1 //0
+  }
+  number = numOriginal; //153
+  var sum = 0;
 
-var numOriginal = 153;
-var num = numOriginal; //0
+  for(item = 0 ; item < digits ; item++){ //0<3 //1<3 //2<3 //3<3
+    var digit = number % 10; // 153%10=> 3, 15%10=>5 //1%10=>1
+    sum = sum + Math.pow(digit,digits); 
+    // Math.pow(3,3) // 0+27=> 27
+    // Math.pow(5,3) // 27+125=> 152
+    // Math.pow(1,3) // 152+1=> 153
+    number = parseInt(number/10); // 15 //1 //0
+  }
 
-var noOfDigits = 0;
+  if(sum == numOriginal){ //153===153
+    console.log(`${numOriginal} is an armstrong number`);
+  }
+  else{
+    console.log(`${numOriginal} is not an armstrong number`);
+  }
 
-while(num!=0){ //153!=0 , 15!=0, 1!=0 , 0!=0
-  noOfDigits++; // 1 // 2 // 3
-  num = parseInt(num/10); // 15 // 1 // 0
 }
 
-// doing your process
-// comparing it with original no 
-console.log(noOfDigits);
-
-
+checkArmstrong(153);
+checkArmstrong(1634);
+checkArmstrong(1004);
 
--- Code/StarPattern.js
+++ Code/StarPattern.js
@@ -1,9 +1,141 @@
-var rows,cols;
+/*
+
+*
+**
+***
+****
+*****
+
+*/ 
+
+// var rows,cols;
+// var output = "";
+// for(rows = 1; rows <= 5 ; rows++){ //rows = 2
+//     for(cols = 1; cols <= rows; cols++){ // cols = 2
+//         output = output + "*"; // //"1\n12"
+//     }
+//     output = output + "\n"; //"1\n22\n"
+// }
+// console.log(output)
+
+
+/* 
+
+*****
+*****
+*****
+*****
+*****
+
+123
+456
+789
+
+987
+654
+321
+
+
+*/
+// var n = 3;
+// var output = "";
+// var num = n*n;
+// for(rows = 0 ; rows < n; rows++){
+//     for(cols = 0; cols < n; cols++){
+//         output = output + num;
+//         num = num - 1;
+//     }
+//     output = output + "\n";
+// }
+// console.log(output)
+
+/* 
+
+*****
+*   *
+*   *
+*   *
+*****
+
+*/
+
+// var n = 7;
+// var output = "";
+
+// for(rows = 0; rows < n; rows++){
+//     for(cols = 0; cols < n; cols++){
+//         if(rows === 0 || rows === n-1){
+//             output = output + "*";
+//         }
+//         else{ // rows = 1,2,3
+//             if(cols === 0 || cols === n-1){ 
+//                 output = output + "*";
+//             }
+//             else{ // cols = 1,2,3
+//                 output = output + " "
+//             }
+//         }
+//     }
+//     output = output + "\n";
+// }
+// console.log(output);
+
+/*
+
+    *
+   **
+  ***
+ ****
+*****   
+
+*/
+
+// var n = 5;
+// var output = "";
+
+// for(rows = 1; rows <= n; rows++){
+//     //printing spaces
+//     for(spaces = 0; spaces < n-rows; spaces++){
+//         output = output + " ";
+//     }
+
+//     //printing stars
+//     for(stars = 0; stars < rows; stars++){
+//         output = output + "*"
+//     }
+//     output = output + "\n"
+// }
+
+// console.log(output)
+
+/*
+
+*
+**
+* *
+*  *
+*****
+
+*/
+
+var n = 6;
 var output = "";
-for(rows = 1; rows <= 5 ; rows++){ //rows = 2
-    for(cols = 1; cols <= rows; cols++){ // cols = 2
-        output = output + cols; // //"1\n12"
+
+for(rows = 1; rows <=n ; rows++){
+    // printing stars
+    for(cols = 0; cols < rows; cols++){
+        console.log(rows,cols)
+        if(rows == n)
+            output = output + "*";
+        else{
+            if(cols == 0 || cols == rows - 1 ){
+                output = output + "*";
+            }
+            else{
+                output = output + " ";
+            }
+        }
     }
-    output = output + "\n"; //"1\n22\n"
+    output = output + "\n"
 }
 console.log(output)
